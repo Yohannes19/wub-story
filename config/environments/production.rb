fa9314866b39897a8170da8c0372e931a43a5d9a
Yohannes19 <=>
2025-04-27 13:47:41 +0200

    adjust env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "wubteret_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'wubstory-3ddeb331c4dd.herokuapp.com' }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   
